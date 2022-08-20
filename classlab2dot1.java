86c54913e357fd9bf48136730ca83ca14b5353c9
Yvangelion <HadenYvan@outlook.com>
2022-08-20 12:54:16 -0400

    init

---
+++ classlab2dot1.java
@@ -0,0 +1,32 @@
+package Lab2dot1;
+
+import java.util.Scanner;
+
+public class classlab2dot1 {
+
+	/* Yvan Ngah
+	 * Computer Science 1 Spring
+	 * 2-1 Convert Celsius to Fahrenheit. 
+	 *
+	 *This program converts Celsius to Fahrenheit in degrees. First the 
+	 *program prompts the user to input the value of Celsius in degrees
+	 *which is then converted into Fahrenheit as the output of the program.
+	 */
+	
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);  		
+
+		System.out.print("Please enter your tempreture in degrees celsius: ");		
+
+		double celsius = input.nextDouble();
+
+		double fahrenheit = (1.8 * celsius) + 32;
+
+		System.out.println("Calculated Temperature in degrees fahrenheit: "+ fahrenheit);
+
+		input.close(); 
+
+	}
+
+}
